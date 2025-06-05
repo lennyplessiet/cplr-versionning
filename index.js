5d0f1cdc4b68562e89a2e4f58153be90eafa582d
lennyplessiet <lennyplessiet@users.noreply.github.com>
2025-06-05 10:01:25 +0000

    deploy: e532a61a82c925a6741539e90e32caa84cb9b246

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+tl = gsap.timeline()
+
 $('.valider').on('click', function(){
+    tl.to('.form', {
+        width: 0,
+        height: 0,
+        padding: 0,
+        opacity:0,
+        duration: 0.3,
+        ease: "ease-in-out"
+    })
+    tl.to('.loader', {
+        opacity: 1,
+        duration: 0.3,
+        delay: 0.2,
+        ease: "ease-in-out"
+    })
     data = {
         "name": $('.nom').val(),
         "firstname": $('.prenom').val(),
@@ -12,7 +28,45 @@ $('.valider').on('click', function(){
         url: "https://n8n.oriatec.fr/webhook-test/88d519cd-fd51-48cb-a7a5-4722d4aedd05",
         data: data,
         success: function (response) {
-            console.log(response)
+            if(response['message'] == "Workflow was started"){
+                setTimeout(function(){
+                    tl.to('.loader', {
+                        opacity: 0,
+                        duration: 0.3,
+                        delay: 0.2,
+                        ease: "ease-in-out"
+                    })
+                    tl.to('.confirmation', {
+                        opacity: 1,
+                        scale: 1.5,
+                        duration: 0.5,
+                        ease: "elastic.out(1,0.3)"
+                    })
+                }, 1000)
+            }else{
+                tl.to('.loader', {
+                    opacity: 1,
+                    duration: 0.3,
+                    delay: 0.2,
+                    ease: "ease-in-out"
+                })
+            }
+        },
+        error: function(){
+            setTimeout(function(){
+                tl.to('.loader', {
+                    opacity: 0,
+                    duration: 0.3,
+                    delay: 0.2,
+                    ease: "ease-in-out"
+                })
+                tl.to('.erreur', {
+                    opacity: 1,
+                    scale: 1.5,
+                    duration: 0.5,
+                    ease: "elastic.out(1,0.3)"
+                })
+            }, 1000)
         }
     });
 })
